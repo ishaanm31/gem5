8d82da2bac91219d099dba975bc01b3686d56861
Yashas M Salian <yashas.msalian@iitb.ac.in>
2024-11-03 16:51:16 +0530

    Routing hints to cerr

--- src/cpu/o3/decode.cc
+++ src/cpu/o3/decode.cc
@@ -297,7 +297,7 @@ Decode::squash(const DynInstPtr &inst, ThreadID tid)
     toFetch->decodeInfo[tid].doneSeqNum = inst->seqNum;
 
     //#YSH -> Print out PC, incorrect branch target, correct branch target and decode/IEW capture flag (0 here)
-    std::cout << inst->pcState().instAddr() << "," << inst->readPredTarg().instAddr() << "," << inst->branchTarget()->instAddr() << ",0" << std::endl;
+    std::cerr << inst->pcState().instAddr() << "," << inst->readPredTarg().instAddr() << "," << inst->branchTarget()->instAddr() << ",0" << std::endl;
     //DPRINTF(BPredHints, "PC: %llu Incorrect Branch Target: %llu Correct Branch Target: %llu\n", inst->pcState(), toFetch->decodeInfo[tid].nextPC, *inst->branchTarget());
 
     set(toFetch->decodeInfo[tid].nextPC, *inst->branchTarget());
